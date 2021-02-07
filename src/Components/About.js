8044d09098282080c6124f49ec8f818274e3b897
deo0072012 <deolucky15@gmail.com>
2021-02-08 03:24:31 +0530

    resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -72,7 +72,7 @@ class About extends Component {
                      </div>
                      <div className="columns download">
                         <p>
-                           <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                           <a href={resumeDownload} className="button"><i download="myFile" className="fa fa-download"></i>Download Resume</a>
                         </p>
                      </div>
                   </div>
