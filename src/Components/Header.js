3f1ba62fc28455468873b45a7fd601233271f9a8
deo0072012 <deolucky15@gmail.com>
2021-02-14 03:01:14 +0530

    dss

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -28,6 +28,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li><a className="smoothscroll" href="https://freetolance-advertisement.netlify.app/">Advertisement</a></li>
          </ul>
 
       </nav>
